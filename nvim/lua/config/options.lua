7e94d2902a153c0a7e588867aa33846e2f75a57a
Bullish-Design <97364799+Bullish-Design@users.noreply.github.com>
2025-12-29 19:49:15 -0500

    Update Neovim options for UI and editing settings

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,16 +1,36 @@
--- Sensible defaults, mostly builtin
+-- nvim/lua/config/options.lua
+
+-- UI
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 vim.opt.signcolumn = "yes"
-vim.opt.mouse = "a"
+vim.opt.cursorline = true
+vim.opt.showmode = false
+vim.opt.laststatus = 3 -- global statusline
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.scrolloff = 4
+vim.opt.sidescrolloff = 8
 
+-- Editing
+vim.opt.mouse = "a"
+vim.opt.wrap = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.updatetime = 200
 vim.opt.timeoutlen = 400
 
-vim.opt.splitbelow = true
-vim.opt.splitright = true
+-- Listchars (subtle whitespace visibility)
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = "→ ",
+  trail = "·",
+  nbsp = "␣",
+}
 
-vim.opt.clipboard = "unnamedplus"
+-- Floating windows
+vim.opt.pumheight = 12
+vim.opt.pumblend = 0
+vim.opt.winblend = 0
+
